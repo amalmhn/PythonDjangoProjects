b64fc0597b22c7fa182a0180052bcc8b0d3190cc
Amal M <amalmhnofficial@gmail.com>
2020-11-25 09:21:04 +0530

    list programs and details

---
+++ python_data_structures/list_programs/list_part1.py
@@ -0,0 +1,44 @@
+lst=['java','python','c#','javascript']
+
+print(lst)
+
+print(lst[0])
+
+print(lst[3])
+
+print(lst[-1])
+
+print(lst[0:3])
+
+#list slicing (upper, lower, step)
+print(lst[0:4:2])
+
+#iteration
+for item in lst:
+    print(item)
+
+#to add a new element to the list
+lst.append("dart")
+
+print(lst)
+
+#UPDATE: to replace a value
+lst[0]='Ruby'
+
+print(lst)
+
+#Can add duplicate values, for eg.
+#lst=(python, python, python)
+
+
+#inserting an object into a specific position
+lst.insert(2, 'perl')
+print(lst)
+
+
+# ...HW...
+# marks=list()
+# insert some values
+# perform slicing
+# update a object
+
---
+++ python_data_structures/python_collections.py
@@ -0,0 +1,2 @@
+names=["ajay",'vijay','tino','john',10,True]
+print(names)
