88c9038f71cb2a19c133354b9c031f0048bf6d8e
Amal M <amalmhnofficial@gmail.com>
2020-11-25 09:36:42 +0530

    hw of list completed

---
+++ python_data_structures/list_programs/hw_marks_list.py
@@ -0,0 +1,13 @@
+marks= [100, 95, 95, 90, 88]
+print(marks)
+
+#insert some values
+marks.insert(2, 70)
+print(marks)
+
+#perform slicing
+print(marks[0:5:2])
+
+#update an object
+marks[2]=90
+print(marks)
