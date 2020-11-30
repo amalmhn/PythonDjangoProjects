fd236e29bde153c970288eb3af8d5326acfaad7f
Amal M <amalmhnofficial@gmail.com>
2020-11-30 21:01:48 +0530

    hw of stack completed

--- python_data_structures/list_programs/stack.py
+++ python_data_structures/list_programs/stack.py
@@ -1,1 +1,19 @@
-#hw stack operation
+stack=[]
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
+
+
+
