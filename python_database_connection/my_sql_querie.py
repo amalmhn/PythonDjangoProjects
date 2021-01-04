971356a63cdb02bc97c4034f56cd8d9a459b2e77
Amal M <amalmhnofficial@gmail.com>
2021-01-04 09:45:50 +0530

    class competed rivision and hw left

---
+++ python_database_connection/my_sql_querie.py
@@ -0,0 +1,13 @@
+# #crud operation
+# create -- c
+# r read
+# u update
+# d delete
+
+#college management system
+#1) create data base for the project
+#listing all databases
+#
+#sq>create database database name;
+
+#tables
